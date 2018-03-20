cb8b4221459cbd70b864931de2f2c4df98381439
Brian Blanchard <briblanch@gmail.com>
2018-03-20 07:43:00 -0700

    barebones app for live coding

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,3 @@
 import 'package:flutter/material.dart';
 
-import 'package:photo_uploader/src/photo_grid.dart';
-
-class PhotoUploaderApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: new ThemeData(
-        primarySwatch: Colors.pink,
-        accentColor: Colors.blueAccent,
-        brightness: Brightness.light,
-      ),
-      home: new PhotoGrid(),
-    );
-  }
-}
+import 'package:photo_uploader/src/photo_list.dart';
--- lib/src/photo_list.dart
+++ lib/src/photo_list.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:uuid/uuid.dart';
 
 import 'package:photo_uploader/src/photo.dart';
+import 'package:photo_uploader/src/uploader.dart';
