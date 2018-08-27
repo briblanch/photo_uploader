a0c6bcae28dea1fdd862e09f48ebd7db7fae632b
Brian Blanchard <briblanch@gmail.com>
2018-08-27 16:08:04 -0700

    clean up

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:photo_uploader/src/colors.dart';
 
+import 'package:photo_uploader/src/colors.dart';
 import 'package:photo_uploader/src/photo_list.dart';
 
 class PhotoUploaderApp extends StatelessWidget {
